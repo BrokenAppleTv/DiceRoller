3023b3ae11c565db6b0ffe67f279fada015477ae
BrokenAppleTv <doubleshotking@gmail.com>
2025-01-29 13:42:12 +1100

    fixed spelling and clear history

--- dice.py
+++ dice.py
@@ -1,4 +1,4 @@
 # put your dice_roll() function in this file
 import random
 def roll_dice(faces):
-    return random.randrange(1, faces)
+    return random.randrange(1, faces + 1)
--- main.py
+++ main.py
@@ -8,13 +8,13 @@ import dice
 
 # GLOBAL (script-wide) variable
 # this stores the selected face option from the drop-down list
-dice_type = 6
+dice_type = 2
 
 
 def select_face_option(event):
     global dice_type  # use global var named dice_type
     ...  # replace with your own code
-    dice_type = int(event.target.vaule)
+    dice_type = int(event.target.value)
 
 
 def roll_all_dice(event):
@@ -25,4 +25,4 @@ def roll_all_dice(event):
 
 def clear_history(event):
     # this finds the div tag with id attribute 'roll-history' and clears whatever is inside
-    document.querySelector("div#roll-history").value = ""
+    document.querySelector("div#roll-history").innerHTML = ""
