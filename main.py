8cf8b3e1365e736132e9b3aab62adce3852dc76b
BrokenAppleTv <doubleshotking@gmail.com>
2025-02-06 11:36:07 +1100

    added multiple dice rolled at once. changed heading, added border

--- main.py
+++ main.py
@@ -20,8 +20,11 @@ def select_face_option(event):
 def roll_all_dice(event):
     global dice_type  # use global var named dice_type
     ...  # replace with your own code
-    result = dice.roll_dice(dice_type)
-    document.querySelector("div#roll-history").innerHTML = "<p id='something'>" + str(result) + "</p>"
+
+    document.querySelector("div#roll-history").innerHTML += "<p id='something'> Rolling dice...</p>"
+    for x in range(1, dice_type + 1):
+        result = dice.roll_dice(dice_type)
+        document.querySelector("div#roll-history").innerHTML += "<p id='something'> :" + str(result) + "</p>"
 
 def clear_history(event):
     # this finds the div tag with id attribute 'roll-history' and clears whatever is inside
